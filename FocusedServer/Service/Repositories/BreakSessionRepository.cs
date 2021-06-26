869008c8d175c9e507839ff1ff1f25039d023d03
YI ZHANG <scaccterran@gmail.com>
2021-06-26 11:09:26 -0400

    FOC-38 ensure all overlapping break sessions are retrieved

--- FocusedServer/Service/Repositories/BreakSessionRepository.cs
+++ FocusedServer/Service/Repositories/BreakSessionRepository.cs
@@ -26,7 +26,22 @@ namespace Service.Repositories
         public async Task<List<BreakSession>> GetBreakSessionByDateRange(string userId, DateTime start, DateTime end)
         {
             var builder = Builders<BreakSession>.Filter;
-            var filter = builder.Eq(_ => _.UserId, userId) & builder.Gte(_ => _.StartTime, start) & builder.Lte(_ => _.EndTime, end);
+            var rangeFilter = builder.Gte(_ => _.StartTime, start) & builder.Lte(_ => _.EndTime, end);
+
+            var startTimeFilter = builder.And(
+                builder.Exists(_ => _.EndTime, false) | builder.Gte(_ => _.EndTime, start),
+                builder.Lte(_ => _.StartTime, start)
+            );
+
+            var endTimeFilter = builder.And(
+                builder.Exists(_ => _.EndTime, false) | builder.Gte(_ => _.EndTime, end),
+                builder.Lte(_ => _.StartTime, end)
+            );
+
+            var filter = builder.And(
+                builder.Eq(_ => _.UserId, userId),
+                builder.Or(rangeFilter, startTimeFilter, endTimeFilter)
+            );
 
             return await Collection.Find(filter).ToListAsync().ConfigureAwait(false);
         }
