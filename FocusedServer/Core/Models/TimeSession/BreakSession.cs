17eeb77967f599fbb8341cc898a404c59f4bafe7
YI ZHANG <scaccterran@gmail.com>
2021-06-26 08:03:54 -0400

    FOC-38 exclude null value in mongo

--- FocusedServer/Core/Models/TimeSession/BreakSession.cs
+++ FocusedServer/Core/Models/TimeSession/BreakSession.cs
@@ -1,3 +1,4 @@
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace Core.Models.TimeSession
@@ -5,6 +6,8 @@ namespace Core.Models.TimeSession
     public class BreakSession : UserOwnedRecord
     {
         public DateTime StartTime { get; set; }
+
+        [BsonIgnoreIfNull]
         public DateTime? EndTime { get; set; }
         public string FocusSessionId { get; set; }
         public double TargetDuration { get; set; }
--- FocusedServer/Core/Models/TimeSession/TimeSeries.cs
+++ FocusedServer/Core/Models/TimeSession/TimeSeries.cs
@@ -1,4 +1,5 @@
 using Core.Enums;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace Core.Models.TimeSession
@@ -6,6 +7,8 @@ namespace Core.Models.TimeSession
     public class TimeSeries : UserOwnedRecord
     {
         public DateTime StartTime { get; set; }
+
+        [BsonIgnoreIfNull]
         public DateTime? EndTime { get; set; }
         public TimeSeriesType Type { get; set; }
         public string DataSourceId { get; set; }
