35b11b40c325c772b0146b704870255f40c72a4a
YI ZHANG <scaccterran@gmail.com>
2021-07-01 17:04:39 -0400

    FOC-36 utilities tests

---
+++ FocusedServer/Services.Test/UnitTests/Utilities/TimeSeriesUtilityTest.cs
@@ -0,0 +1,77 @@
+using Core.Models.TimeSession;
+using NUnit.Framework;
+using Service.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services.Test.UnitTests.Utilities
+{
+    [TestFixture]
+    public class TimeSeriesUtilityTest
+    {
+        private List<TimeSeries> _series;
+
+        [SetUp]
+        public void Setup()
+        {
+            _series = new List<TimeSeries>
+            {
+                new TimeSeries
+                {
+                    StartTime = new DateTime(2021, 1, 5, 5, 15, 0),
+                    EndTime = new DateTime(2021, 1, 5, 12, 15, 0)
+                },
+                new TimeSeries
+                {
+                    StartTime = new DateTime(2021, 1, 5, 12, 15, 0),
+                    EndTime = new DateTime(2021, 1, 5, 12, 45, 0)
+                },
+                new TimeSeries
+                {
+                    StartTime = new DateTime(2021, 1, 6, 12, 15, 0),
+                    EndTime = new DateTime(2021, 1, 6, 13, 15, 0)
+                },
+                new TimeSeries
+                {
+                    StartTime = new DateTime(2021, 1, 7, 12, 15, 0),
+                    EndTime = new DateTime(2021, 1, 7, 12, 45, 0)
+                }
+            };
+        }
+
+        [Test]
+        public void GetTotalTimeShouldReturnZeroWhenTimeSeriesIsEmpty()
+        {
+            Assert.AreEqual(0, TimeSeriesUtility.GetTotalTime(new List<TimeSeries>(), new DateTime(2020, 1, 1), DateTime.UtcNow));
+        }
+
+        [Test]
+        public void GetTotalTimeShouldReturnTimeWithinTimeRange()
+        {
+            var start = new DateTime(2021, 1, 5, 12, 15, 0);
+            var end = new DateTime(2021, 1, 7, 12, 15, 0);
+
+            Assert.AreEqual(1.5, TimeSeriesUtility.GetTotalTime(_series, start, end));
+        }
+
+        [Test]
+        public void GetTotalTimeShouldIncludeOverlappingTime()
+        {
+            var start = new DateTime(2021, 1, 5, 10, 15, 0);
+            var end = new DateTime(2021, 1, 7, 12, 30, 0);
+
+            Assert.AreEqual(3.75, TimeSeriesUtility.GetTotalTime(_series, start, end));
+        }
+
+        [Test]
+        public void GetTotalTimeShouldIncludeOpenTimeRange()
+        {
+            _series.Last().EndTime = null;
+            var start = DateTime.UtcNow.AddHours(-3);
+            var end = DateTime.UtcNow;
+
+            Assert.AreEqual(3, Math.Round(TimeSeriesUtility.GetTotalTime(_series, start, end), 2));
+        }
+    }
+}
---
+++ FocusedServer/Services.Test/UnitTests/Utilities/WorkItemUtilityTest.cs
@@ -0,0 +1,100 @@
+using Core.Enums;
+using Core.Models.Aggregates;
+using Core.Models.Generic;
+using Core.Models.TimeSession;
+using Core.Models.WorkItem;
+using NUnit.Framework;
+using Service.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace Services.Test.UnitTests.Utilities
+{
+    [TestFixture]
+    public class WorkItemUtilityTest
+    {
+        [Test]
+        public void ToWorkItemDtoShouldReturnIncompletedProgression()
+        {
+            var item = new WorkItemWithTimeSeries
+            {
+                Status = WorkItemStatus.Highlighted,
+                EstimatedHours = 5,
+                TimeInfo = new TimeInfo { Created = new DateTime(2021, 1, 1) },
+                TimeSeries = new List<TimeSeries>
+                {
+                    new TimeSeries
+                    {
+                        StartTime = new DateTime(2021, 1, 25, 5, 15, 0),
+                        EndTime = new DateTime(2021, 1, 25, 12, 15, 0)
+                    }
+                },
+                Subtasks = new List<WorkItem>
+                {
+                    new WorkItem { Status = WorkItemStatus.Highlighted },
+                    new WorkItem { Status = WorkItemStatus.Completed }
+                },
+                Checklist = new List<ChecklistEntry>
+                {
+                    new ChecklistEntry { IsCompleted = true },
+                    new ChecklistEntry { IsCompleted = false },
+                    new ChecklistEntry { IsCompleted = true }
+                }
+            };
+
+            var result = WorkItemUtility.ToWorkItemDto(item);
+
+            Assert.AreEqual(7, result.ItemProgress.Current);
+            Assert.AreEqual(5, result.ItemProgress.Target);
+            Assert.IsFalse(result.ItemProgress.IsCompleted);
+            Assert.AreEqual(1, result.SubtaskProgress.Current);
+            Assert.AreEqual(2, result.SubtaskProgress.Target);
+            Assert.IsFalse(result.SubtaskProgress.IsCompleted);
+            Assert.AreEqual(2, result.ChecklistProgress.Current);
+            Assert.AreEqual(3, result.ChecklistProgress.Target);
+            Assert.IsFalse(result.ChecklistProgress.IsCompleted);
+        }
+
+        [Test]
+        public void ToWorkItemDtoShouldReturnCompletedProgression()
+        {
+            var item = new WorkItemWithTimeSeries
+            {
+                Status = WorkItemStatus.Completed,
+                EstimatedHours = 5,
+                TimeInfo = new TimeInfo { Created = new DateTime(2021, 1, 1) },
+                TimeSeries = new List<TimeSeries>
+                {
+                    new TimeSeries
+                    {
+                        StartTime = new DateTime(2021, 1, 25, 5, 15, 0),
+                        EndTime = new DateTime(2021, 1, 25, 12, 15, 0)
+                    }
+                },
+                Subtasks = new List<WorkItem>
+                {
+                    new WorkItem { Status = WorkItemStatus.Completed },
+                    new WorkItem { Status = WorkItemStatus.Completed }
+                },
+                Checklist = new List<ChecklistEntry>
+                {
+                    new ChecklistEntry { IsCompleted = true },
+                    new ChecklistEntry { IsCompleted = true },
+                    new ChecklistEntry { IsCompleted = true }
+                }
+            };
+
+            var result = WorkItemUtility.ToWorkItemDto(item);
+
+            Assert.AreEqual(7, result.ItemProgress.Current);
+            Assert.AreEqual(5, result.ItemProgress.Target);
+            Assert.IsTrue(result.ItemProgress.IsCompleted);
+            Assert.AreEqual(2, result.SubtaskProgress.Current);
+            Assert.AreEqual(2, result.SubtaskProgress.Target);
+            Assert.IsTrue(result.SubtaskProgress.IsCompleted);
+            Assert.AreEqual(3, result.ChecklistProgress.Current);
+            Assert.AreEqual(3, result.ChecklistProgress.Target);
+            Assert.IsTrue(result.ChecklistProgress.IsCompleted);
+        }
+    }
+}
