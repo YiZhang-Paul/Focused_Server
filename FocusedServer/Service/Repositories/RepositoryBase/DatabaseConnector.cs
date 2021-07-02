a2d818e7081a93d10909ea9f45d386d4bb51896d
YI ZHANG <scaccterran@gmail.com>
2021-07-02 14:51:30 -0400

    FOC-36 allow dropping collection

--- FocusedServer/Service/Repositories/RepositoryBase/DatabaseConnector.cs
+++ FocusedServer/Service/Repositories/RepositoryBase/DatabaseConnector.cs
@@ -2,23 +2,29 @@ using Core.Configurations;
 using Core.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using System.Threading.Tasks;
 
 namespace Service.Repositories.RepositoryBase
 {
     public class DatabaseConnector<T> where T : DatabaseEntry
     {
-        private DatabaseConfiguration Configuration { get; set; }
         protected IMongoCollection<T> Collection { get; set; }
+        private IMongoDatabase Database { get; set; }
 
         public DatabaseConnector(IOptions<DatabaseConfiguration> configuration, string collection)
         {
-            Configuration = configuration.Value;
+            Database = new MongoClient(configuration.Value.Url).GetDatabase(configuration.Value.Name);
             Collection = Connect<T>(collection);
         }
 
         protected IMongoCollection<K> Connect<K>(string collection) where K : DatabaseEntry
         {
-            return new MongoClient(Configuration.Url).GetDatabase(Configuration.Name).GetCollection<K>(collection);
+            return Database.GetCollection<K>(collection);
+        }
+
+        public async Task DropCollection()
+        {
+            await Database.DropCollectionAsync(typeof(T).Name).ConfigureAwait(false);
         }
     }
 }
